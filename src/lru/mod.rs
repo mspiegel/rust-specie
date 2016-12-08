bfda53601ab10543bbc6a40d224a396b46a28933
Michael Spiegel <michael.m.spiegel@gmail.com>
2016-12-08 14:25:43 -0500

    Add documentation.

--- src/lru/mod.rs
+++ src/lru/mod.rs
@@ -1,3 +1,6 @@
+//! The 'lru' module implements a [least-recently used](
+//! https://en.wikipedia.org/wiki/Cache_replacement_policies#Least_Recently_Used_.28LRU.29) cache.
+
 use std::collections::BTreeMap;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
@@ -5,14 +8,20 @@ use std::hash::Hash;
 use std::rc::Rc;
 
 struct CacheEntry<V> {
+    // cache value
     val: V,
+    // clock instant when entry was most recently accessed
     instant: u64,
 }
 
 pub struct LRUCache<K: Eq + Hash, V> {
+    // maximum number of elements stored in the cache
     capacity: usize,
+    // logical clock that is incremented on each operation
     clock: u64,
+    // unordered map that stores (key, value) pairs
     data: HashMap<Rc<K>, CacheEntry<V>>,
+    // ordered map sorted by clock instants. Used by eviction algorithm
     order: BTreeMap<u64, Rc<K>>,
 }
 
@@ -32,6 +41,9 @@ impl<K, V> LRUCache<K, V>
         let now = self.clock;
         let key = Rc::new(key);
         let prev = match self.data.entry(key.clone()) {
+            // If the (key, value) pair is located,
+            // then find the logical time instant associated
+            // with that key. The instant must be set to now.
             Entry::Occupied(mut e) => {
                 self.clock += 1;
                 let e = e.get_mut();
@@ -43,6 +55,9 @@ impl<K, V> LRUCache<K, V>
         };
         match prev {
             Some(t) => {
+                // If the (key, value) pair is located,
+                // then delete the association with the old instant
+                // and create an association to now.
                 let k = self.order.remove(&t);
                 self.order.insert(now, k.unwrap());
                 self.data.get(key.as_ref()).as_ref().map(|x| &x.val)
@@ -57,6 +72,10 @@ impl<K, V> LRUCache<K, V>
         let size = self.data.len();
         let key = Rc::new(key);
         let evict = match self.data.entry(key.clone()) {
+            // If the (key, value) pair is located,
+            // then replace the previous value,
+            // and update the logical time association
+            // with the pair.
             Entry::Occupied(mut e) => {
                 let e = e.get_mut();
                 let prev = e.instant;
@@ -66,22 +85,30 @@ impl<K, V> LRUCache<K, V>
                 self.order.insert(now, key.clone());
                 None
             }
+            // If the (key, value) pair is not located,
+            // then insert the new association.
             Entry::Vacant(e) => {
+                let evict = {
+                    if size == self.capacity {
+                        // Evict the oldest entry from the clock instant map
+                        let oldest = self.order.keys().cloned().next().unwrap();
+                        Some(self.order.remove(&oldest).unwrap())
+                    } else {
+                        None
+                    }
+                };
                 let entry = CacheEntry {
                     val: val,
                     instant: now,
                 };
                 e.insert(entry);
                 self.order.insert(now, key.clone());
-                if size == self.capacity {
-                    let oldest = self.order.keys().cloned().next().unwrap();
-                    Some(self.order.remove(&oldest).unwrap())
-                } else {
-                    None
-                }
+                evict
             }
         };
         match evict {
+            // Evict the oldest entry from the data map
+            // Moved to end of function because of borrow checker
             Some(k) => {
                 self.data.remove(k.as_ref());
             }
